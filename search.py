228d36b3ad7e25a92132d3fd395767d32512ef74
Mongush Nachyn <mongushnv@gmail.com>
2020-12-14 02:14:32 +0300

    Code refactoring and performance improvements

--- search.py
+++ search.py
@@ -1,57 +1,58 @@
-import re
-import io
+import time
 
 
-def line_conversion(in_line):
-
-    start_ad = 'test_domain.ru'
-    start_ln = 'exchange'
-    in_line = re.sub(r':', '', in_line[:-2])
-    in_line = re.sub(r'"', "'", in_line)
-    x = in_line.lower().index('пароль') + 7
-
-    if start_ad in in_line:
-        login = in_line[in_line.index('MSK\\')+4:]
-        login = login[:login.index("'")]
-        return (login + ' ' + in_line[x:]).split()
-    elif start_ln in in_line:
-        return ('exchange ' + in_line[x:]).split()
-
-
-def writing_to_result_a_file(names, founds_names):
-
-    f = open('output.txt', 'w')
-    f.write('----  НЕ НАЙДЕНЫ   ----\n\n')
-    for name in names:
-        if (founds_names.get(name, False) == False):
-            f.write(name + '\n')
-
-    f.write('\n\n')
-
-    f.write('----  НАЙДЕНЫ   ----\n\n')
-    for key, val in founds_names.items():
-        f.write(key + ' ' + str(val) + '\n')
-    f.close()
-
-
-def main():
-
-    with open("names.txt", encoding="utf-8-sig") as file_name:
-        names = tuple([line.rstrip() for line in file_name])
+def data_search(in_line):
+    in_line = in_line[:-2].replace(r':', '')
+    in_line = in_line.replace(r"'", '"')
+    in_line = in_line.replace(r",", "")
+    in_line = in_line.split('"')
     
-    search_line = "В ресурсе"
-    names_dict = {}
-    with io.open("input.txt", encoding="utf-8") as password_file:
-        for line in password_file:
-            if search_line in line:
-                for name in names:
-                    if name in line:
-                        try:
-                            names_dict[name] += line_conversion(line)
-                        except KeyError:
-                            names_dict[name] = line_conversion(line)
+    name = in_line[3].strip()
+    login = in_line[5]
+    password = in_line[6].split()[1]
+    return name, login, password
 
-    writing_to_result_a_file(names, names_dict)
+
+def save_to_file(data: dict):
+    with open("names.txt", encoding="utf-8-sig") as file_with_names:
+        names = tuple([name.strip() for name in file_with_names if name.strip()])
+    
+    not_found = []
+    with open('output.txt', 'w', encoding="utf-8") as f:
+        f.write('----   НАЙДЕНЫ   ----\n\n')
+        for name in names:
+            try:
+                f.write(f"{name} [ {data[name]} ]\n")
+            except KeyError:
+                not_found.append(name)
+
+        f.write('\n\n----   НЕ НАЙДЕНЫ   ----\n\n')
+        for name in not_found:
+            f.write(f"{name}\n")
+
+
+def run_time_print(func):
+    def wrapped():
+        start_time = time.time()
+        func()
+        print(f"Время выполнения: {time.time() - start_time} секунд")
+    return wrapped
+
+
+@run_time_print
+def main():
+    search_line = "В ресурсе"
+    passwords_dict = {}
+    with open("input.txt", encoding="utf-8-sig") as password_file:
+        for line in password_file:
+            line = line.lstrip()
+            if line.startswith(search_line):
+                name, login, password = data_search(line)
+                try:
+                    passwords_dict[name] += (f' "{login}" "{password}"')
+                except KeyError:
+                    passwords_dict[name] = (f'"{login}" "{password}"')
+    save_to_file(passwords_dict)
 
 
 if __name__ == "__main__":
